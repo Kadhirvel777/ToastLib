020bfb542abbc96bed52a76a116c1260f9018985
Kadhirvel <Kadhirvel.n@zensar.com>
2022-08-02 13:54:01 +0530

    Added pod spec and source files

---
+++ ToastLib/ToastLib.h
@@ -0,0 +1,18 @@
+//
+//  ToastLib.h
+//  ToastLib
+//
+//  Created by Kadhirvel on 02/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToastLib.
+FOUNDATION_EXPORT double ToastLibVersionNumber;
+
+//! Project version string for ToastLib.
+FOUNDATION_EXPORT const unsigned char ToastLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastLib/PublicHeader.h>
+
+
